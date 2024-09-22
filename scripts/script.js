c286bba6b203c3a389f78fe2ee911267e75b409d
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-09-22 11:07:29 -0400

    tweaked menu and corrected code

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,58 @@
-// Select and cache the <main> element in a variable named mainEl.
-let mainEl = document.querySelector('main');
-
-// Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
-// Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
-mainEl.style.backgroundColor = 'var(--main-bg)';
-
-// Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do
-// this; pick whichever one that you think works best in this situation.
-mainEl.innerHTML = `<h1>DOM Manipulation</h1>`;
-
-// Add a class of flex-ctr to mainEl. // Hint: Use the Element.classList API.
-mainEl.classList.add(`flex-ctr`);
-
-// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
-let topMenuE1 = document.getElementById(`top-menu`);
-console.log(topMenuEl)
-// Set the height of the topMenuEl element to be 100%.
+// Menu Link Data
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+];
 
 
-// Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom
-// property.
+//-------- Part 1 -------------------------------------------------//
+
+// 1. Select and cache the <main> element in a variable named mainEl.
+let mainEl = document.getElementsByTagName('main');
+console.log(mainEl[0]);
+
+// 2. Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+mainEl[0].style.backgroundColor = 'var(--main-bg)';
+
+// 3. Set the content of mainEl to <h1>DOM Manipulation</h1>.
+mainEl[0].innerHTML = '<h1>DOM Manipulation</h1>';
+
+// 4. Add a class of flex-ctr to mainEl. Hint: Use the Element.classList API.
+mainEl[0].classList.add('flex-ctr');
 
 
-// Add a class of flex-around to topMenuEl.
+//-------- Part 2 -------------------------------------------------//
+
+// 1. Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+let topMenuEl = document.getElementById('top-menu');
+
+// 2. Set the height of the topMenuEl element to be 100%.
+topMenuEl.style.height = '100%';
+
+// 3. Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+// 4. Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add('flex-around');
+
+
+// ------- Part 3 ------------------------------------------------//
+
+// 1. Iterate over the entire menuLinks array and for each "link" object:
+menuLinks.forEach((Link) => {
+
+    // 2. Create an <a> element - .createElement()
+    let newLink = document.createElement(`a`);
+
+    // 3. On the new element, add an href attribute with its value set to the href property of the "link" object.
+    newLink.setAttribute(`href`, Link.href);
+    // console.Log(newLink)
+
+    // 4. Set the new element's content to the value of the text property of the "link" object.
+    newLink.textContent = Link.text;
+
+    // 5. Append the new element to the topMenuEl element.
+    topMenuEl.appendChild(newLink);
+})
